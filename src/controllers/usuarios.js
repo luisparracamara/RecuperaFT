84c3fe6d1532568998165588cd321930a208b7e2
Luis <luisparracamara@gmail.com>
2019-10-12 17:30:30 -0700

    primer commit

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -87,40 +87,8 @@ ctrl.loginUsuario = async(req,res) => {
     try {
         const loginUsuario = await Usuario.findOne({ username: req.body.username }).exec();
 
-        
-        if (loginUsuario<1) {
-            return res.status(401).json({
-                message: "La contraseña o usuario no coinciden",
-                
-            });
-        }
-
-
-        try {
-            const autentificacion = bcrypt.compareSync(req.body.password, loginUsuario.password);
-            
-            if (autentificacion===false) {
-
-               const token = jwt.sign({
-                    username: loginUsuario.username,
-                    id: loginUsuario._id,
-                    rol: loginUsuario.rol,
-                }, process.env.JWT_KEY, { expiresIn: '1h' })
-        
-                return res.status(200).json({
-                    message: "Autenticación válida",
-                    token
-                });
-            }else{
-                return res.status(401).json({
-                    message: "La contraseña o usuario no coinciden"
-                });
-            }
-        } catch (error) {
-            return res.status(401).json({
-                message: "La contraseña o usuario no coinciden",
-            });
-        }
+        res.send(loginUsuario.length)
+      
     
     } catch (error) {
         return res.status(500).json({
